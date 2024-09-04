8fda435c6be84789931c95865a63a77e7ba4ff06
pabalberione <pabalberione@gmail.com>
2024-09-04 15:05:14 -0300

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pabalberione/godesde0
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/pabalberione/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
